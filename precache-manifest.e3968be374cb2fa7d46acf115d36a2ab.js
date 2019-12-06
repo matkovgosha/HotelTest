9905e8f596c26dcc6b9b0713d8750801014782ed
Gosha <matkovgoshka@yandex.ru>
2019-12-07 01:39:52 +0400

    Updates

---
+++ precache-manifest.e3968be374cb2fa7d46acf115d36a2ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "924d77c6d8c487a9deb9b879b5824ecb",
+    "url": "/HotelTest/static/media/Head.924d77c6.png"
+  },
+  {
+    "revision": "0867bed4e06e8e614896",
+    "url": "/HotelTest/static/js/runtime~main.0867bed4.js"
+  },
+  {
+    "revision": "280310714b172706adeb",
+    "url": "/HotelTest/static/js/main.28031071.chunk.js"
+  },
+  {
+    "revision": "a723ec424450fba42085",
+    "url": "/HotelTest/static/js/1.a723ec42.chunk.js"
+  },
+  {
+    "revision": "280310714b172706adeb",
+    "url": "/HotelTest/static/css/main.41089288.chunk.css"
+  },
+  {
+    "revision": "075dad15b30ea0d6b02e67ef9cde3b23",
+    "url": "/HotelTest/index.html"
+  }
+];
