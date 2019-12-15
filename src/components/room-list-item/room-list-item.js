46c3653eb13b8bb9fd157c8a08d06acf5abefaec
Gosha <matkovgoshka@yandex.ru>
2019-12-15 18:51:01 +0400

    rooms almost ready

--- src/components/room-list-item/room-list-item.js
+++ src/components/room-list-item/room-list-item.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const RoomListItem = ({ room }) => {
-  const { id, title, price, coverImage } = room;
+  const { id, title, price, listImage } = room;
 
   const newPath = "room/" + id;
 
 
   return (
         <div className="item">
-          <img src = {coverImage} alt="Room"/>
+          <img src = {listImage} alt="Room"/>
           <p className ="room-name">{title}</p>
           <div className="room-bonus"><img src = "img/Wifi.svg" alt="Wi-Fi"/>Wi-Fi</div>
           <div className="room-bonus"><img src = "img/Food.svg" alt="Food"/>3x разовое</div>
